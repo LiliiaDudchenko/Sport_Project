5cdeb0338f56eb5d5caf4db4a8deabc44404d288
LiliiaDudchenko <dudchenkolilia@gmail.com>
2019-12-06 17:21:05 +0200

    homework15
    Added footer + responsive

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,17 @@
 $('.navigation').on('click', '.mobile_menu', function () {
     $(".navigation ul").slideToggle(400);
 });
+
+
+
+(function($) {
+$(function() {
+ 
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+ 
+});
+})(jQuery);

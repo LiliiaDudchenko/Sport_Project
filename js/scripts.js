2c2e1d9fbb3962c8fbbc3ca8cf25bf090729cd23
LiliiaDudchenko <dudchenkolilia@gmail.com>
2020-01-22 13:31:28 +0200

    added resize function

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,31 @@
-// $(".mobile_menu").click(function () {
-//     $(".navigation ul").slideToggle(400);
-// });
-
-
 $('.navigation').on('click', '.mobile_menu', function () {
-    $(".navigation ul").slideToggle(400);
+  $(".navigation ul").slideToggle(400);
 });
 
 
 
-(function($) {
-$(function() {
- 
-  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-    $(this)
-      .addClass('active').siblings().removeClass('active')
-      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+(function ($) {
+  $(function () {
+
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+      $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+
   });
- 
-});
 })(jQuery);
+
+jQuery(window).resize(function () {
+  const windowWidth = $(window).width();
+  console.log(windowWidth);
+  if (windowWidth > 768) {
+    $('.navigation ul').css({
+      display: 'flex'
+    })
+  } else {
+    $('.navigation ul').css({
+      display: 'none'
+    })
+  }
+});
